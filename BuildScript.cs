22a7af771cc36963a69797622e60fa0fde1c0bf8
nolimitk <nolimitk@gmail.com>
2025-04-26 06:53:40 +0900

    scene name changed

--- BuildScript.cs
+++ BuildScript.cs
@@ -4,7 +4,7 @@ public class BuildScript
 {
     public static void BuildWebGL()
     {
-        string[] scenes = { "Assets/lobby.unity" }; // 빌드할 씬
+        string[] scenes = { "Assets/VRLearningScene.unity" }; // 빌드할 씬
         string outputPath = "Build/WebGL";
 
         BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.WebGL, BuildOptions.None);
