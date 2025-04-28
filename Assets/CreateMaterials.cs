6914fee249d301db669cb4864b8e94dc2362a044
nolimitk <nolimitk@gmail.com>
2025-04-29 00:07:58 +0900

    360 images
    navigation menu에서 360image 이동

---
+++ Assets/CreateMaterials.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEditor;
+
+public class ImageTool : MonoBehaviour
+{
+    [MenuItem("Tools/Create Materials for 360 Images")]
+    static void CreateMaterials()
+    {
+        string texturePath = "Assets/360Images"; // 이미지 파일 경로
+        string[] filePaths = System.IO.Directory.GetFiles(texturePath, "*.jpg"); // JPG 파일 검색 (다른 형식이면 수정)
+
+        foreach (string filePath in filePaths)
+        {
+            string fileName = System.IO.Path.GetFileNameWithoutExtension(filePath);
+
+            // 새로운 머티리얼 생성
+            Material material = new Material(Shader.Find("Unlit/ReverseTexture"));
+            Texture2D texture = AssetDatabase.LoadAssetAtPath<Texture2D>(filePath);
+            material.SetTexture("_MainTex", texture);
+
+            // 머티리얼 저장
+            AssetDatabase.CreateAsset(material, $"Assets/360Images/Materials/{fileName}.mat");
+            Debug.Log($"{fileName} Material Created");
+        }
+
+        AssetDatabase.Refresh();
+    }
+}
--- Assets/Transition.cs
+++ Assets/Transition.cs
@@ -1,4 +1,9 @@
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using UnityEditor;
 
 public class Transition : MonoBehaviour
 {
@@ -8,29 +13,100 @@ public class Transition : MonoBehaviour
     public GameObject mapMenuPanel;
     public GameObject controlPanel;
 
+    [SerializeField]
+    TextAsset partImagesCSV;
+
+    List<PartData> partData;
+
+    public Button[] naviagionButtons;
+
+    public GameObject sphere; // 구체 오브젝트
+
+
     public void OpenTrain360()
     {
         launcherCanvas.SetActive(false);
         train360.SetActive(true);
     }
-
-    public void openTrainPart()
+    public void openTrainPart(int index)
     {
         mainPanel.SetActive(false);
         mapMenuPanel.SetActive(false);
-        // TODO : 부품에 따라서 내용이 변경되어야 한다.
         controlPanel.SetActive(true);
+
+        ChangeSphereMaterial(index);
+    }
+
+    void ChangeSphereMaterial(int index)
+    {
+        string materialName = partData[index].partImageFileName.Replace(".jpg", "");
+        materialName = materialName + ".mat";
+
+        Debug.Log($"load material : {partData[index].partNumber} {materialName} {partData[index].partImageFileName} {partData[index].linkImageFileName}");
+        Material newMaterial = AssetDatabase.LoadAssetAtPath<Material>($"Assets/360images/Materials/{materialName}");
+
+        if (newMaterial == null)
+        {
+            Debug.LogError("Material not found at: " + materialName);
+            return;
+        }
+        sphere.GetComponent<Renderer>().material = newMaterial; // 머터리얼 적용
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        partData = CsvLoader< PartData >.LoadData(partImagesCSV);
+
+        for (int i = 0; i < naviagionButtons.Length; i++)
+        {
+            //Debug.Log($"part number : {partData[i].partNumber} {partData[i].partImageFileName} {partData[i].linkImageFileName}");
+            int index = i;
+            naviagionButtons[i].onClick.AddListener(() => openTrainPart(index));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+}
+
+public abstract class CSVData
+{
+    public abstract void SetData(string[] data);
+}
+
+public class PartData : CSVData
+{
+    public int partNumber { get; set; }
+    public string partImageFileName { get; set; }
+    public string linkImageFileName { get; set; }
+
+    public override void SetData(string[] data)
+    {
+        partNumber = int.Parse(data[0]);
+        partImageFileName = data[1];
+        linkImageFileName = data[2];
+    }
+}
+
+public class CsvLoader<TCSVData> where TCSVData : CSVData, new()
+{
+    public static List<TCSVData> LoadData(TextAsset csvAsset)
+    {
+        var data = new List<TCSVData>();
+        var reader = new StringReader(csvAsset.text);
+
+        while (reader.Peek() > -1)
+        {
+            var line = reader.ReadLine();
+            var csvdata = new TCSVData();
+            csvdata.SetData(line.Split(','));
+            data.Add(csvdata);
+        }
+
+        return data;
     }
 }
